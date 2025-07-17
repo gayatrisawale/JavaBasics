5d9a5da7e34b957d79ac9ab7d85e6b80d20e1187
Gayatri <gayatrisawale.gs@gmail.com>
2025-07-17 10:24:26 +0530

    AssignmentOperaterIFElse

---
+++ AssignmentIfElseOperator/src/Assignment.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Assignment {
+    public static void main(String[] args) {
+        int num1=2;
+        int num2=1;
+        int num3;
+        System.out.println( num1 +","+num2);
+        num3=num2;
+        num2=num1;
+        num1=num3;
+        System.out.println( num1 +","+num2);
+    }
+}
+
+
+
+
+
+
+//What steps do you think individuals can take to reduce their carbon footprint in daily life?
---
+++ AssignmentIfElseOperator/src/TestOp.java
@@ -0,0 +1,41 @@
+import java.lang.*;
+import java.util.Scanner;
+public class TestOp {
+    public static void main(String[] args) {
+
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Run the test");
+        System.out.println("--------ADDITION----------");
+        System.out.println("Take elements value");
+        int v1=scn.nextInt();
+        int v2=scn.nextInt();
+        int add= v1+v2;
+        System.out.println("Result"+ add);
+//        System.out.println("--------SUBSTARCTION----------");
+//        float v1=scn.nextInt();
+//        float v2=scn.nextInt();
+//        float sub= v1+v2;
+//        System.out.println("Result"+ sub);
+//        System.out.println("--------MULTIPLICATION----------");
+//        int v1=scn.nextInt();
+//        int v2=scn.nextInt();
+//        int add= v1+v2;
+//        System.out.println("Result"+ add);
+//        System.out.println("--------DEVISION----------");
+//        int v1=scn.nextInt();
+//        int v2=scn.nextInt();
+//        int add= v1+v2;
+//        System.out.println("Result"+ add);
+//        System.out.println("--------MOD----------");
+//        int v1=scn.nextInt();
+//        int v2=scn.nextInt();
+//        int add= v1+v2;
+//        System.out.println("Result"+ add);
+//        System.out.println("---------------------------");
+
+
+
+
+
+    }
+}
